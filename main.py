205c397d01f847745fabc579d2ce479d93ba551e
MTopal70 <85746681+MTopal70@users.noreply.github.com>
2025-06-11 13:14:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("Masterschool workshop")
 print("We have already cloned this repo locally")
 print("Another change")
 print("This changes happend trough Github")
+print("This changes also happend trough Github")
 
 
 
